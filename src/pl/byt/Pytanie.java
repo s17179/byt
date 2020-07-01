ce47bd0de87daeab042e41ad57810c82661b7779
Dominik <kontakt@dominikgasior.pl>
2020-07-01 21:54:28 +0200

    add Odpowiedz

--- src/pl/byt/Pytanie.java
+++ src/pl/byt/Pytanie.java
@@ -1,9 +1,17 @@
 package pl.byt;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Pytanie {
     private String tresc;
 
     private TestKompetencji testKompetencji;
+
+    private List<Odpowiedz> odpowiedzs = new ArrayList<>();
+    public static Set<Odpowiedz> allOdpowiedzs = new HashSet<>();
 
     private Pytanie(TestKompetencji testKompetencji) {
         this.testKompetencji = testKompetencji;
@@ -19,5 +27,50 @@ public class Pytanie {
         testKompetencji.addPytanie(pytanie);
 
         return pytanie;
+    }
+
+    public void addOdpowiedz(Odpowiedz odpowiedz) throws Exception {
+        if (hasAtLeastOneCorrectAnswer() && odpowiedz.isPoprawna()) {
+            throw new Exception("There can be only one correct answer");
+        }
+
+        if (odpowiedzs.size() == 3 && !hasAtLeastOneCorrectAnswer() && !odpowiedz.isPoprawna()) {
+            throw new Exception("There must at least on correct answer");
+        }
+
+        if (odpowiedzs.size() == 4) {
+            throw new Exception("Size of odpowiedzs cannot be greater than 4");
+        }
+
+        if (!odpowiedzs.contains(odpowiedz)) {
+            if (allOdpowiedzs.contains(odpowiedz)) {
+                throw new Exception("Given odpowiedz is already connected with a RekrutacjaMediator");
+            }
+
+            odpowiedzs.add(odpowiedz);
+            allOdpowiedzs.add(odpowiedz);
+        }
+    }
+
+    public String getTresc() {
+        return tresc;
+    }
+
+    public TestKompetencji getTestKompetencji() {
+        return testKompetencji;
+    }
+
+    public List<Odpowiedz> getOdpowiedzs() {
+        return odpowiedzs;
+    }
+
+    private boolean hasAtLeastOneCorrectAnswer() {
+        for (Odpowiedz odpowiedz : odpowiedzs) {
+            if (odpowiedz.isPoprawna()) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
